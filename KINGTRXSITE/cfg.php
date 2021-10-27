f6a252a43fa07482780d603457413cda33cd631a
nguyenaman23 <91823952+nguyenaman23@users.noreply.github.com>
2021-10-27 11:07:49 +0700

    Add files via upload

---
+++ KINGTRXSITE/cfg.php
@@ -0,0 +1,7 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36";
+
+$cookie = "PHPSESSID=2b33e4222706e9618de57d0e440160f9; address_TRX=TDBxnFnTjspLNU4BjQbHpVwxifGXbfGXEa";
+
+$wallet = "TDBxnFnTjspLNU4BjQbHpVwxifGXbfGXEa";
